452548a7f185d42369b438e1fe389113b57d5831
Moshe Kolodny <kolodny@github.com>
2013-11-28 15:01:23 -0500

    first commit

--- config.rb
+++ config.rb
@@ -4,11 +4,11 @@ require 'sass-globbing'
 project_type = :stand_alone
 
 # Publishing paths
-http_path = "/"
-http_images_path = "/images"
+http_path = "/octopress-demo3/"
+http_images_path = "/octopress-demo3/images"
 http_generated_images_path = "/images"
-http_fonts_path = "/fonts"
-css_dir = "public/stylesheets"
+http_fonts_path = "/octopress-demo3/fonts"
+css_dir = "public/octopress-demo3/stylesheets"
 
 # Local development paths
 sass_dir = "sass"
---
+++ source/_posts/2013-11-28-first-post.markdown
@@ -0,0 +1,8 @@
+---
+layout: post
+title: "First Post"
+date: 2013-11-28 15:01:17 -0500
+comments: true
+categories: 
+---
+This is the first post
